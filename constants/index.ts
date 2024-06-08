8929c5e04665660988f1723cfd1d64272c7f430f
Argha Das <arghadas@Arghas-MacBook-Air.local>
2024-06-08 18:17:40 +0530

    adding server url

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,4 @@
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000"
+    : "https://mern-booking-app-backend-qo9a.onrender.com";
